8f6a6c1bdb353fadc4db5c4c3c29ad75ff1cf982
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-02 18:25:48 -0400

    Updated from Brython Server: 10/2/2018 6:25:51 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 JumbleAyah = input("Please enter a string of text (that bigger the better): " )
-print("You entered " + ""+ JumbleAyah + ". Now jumble it:")
+print("You entered "+ JumbleAyah +". Now jumble it:")
 def reverse(s): 
   str = "" 
   for i in s: 
